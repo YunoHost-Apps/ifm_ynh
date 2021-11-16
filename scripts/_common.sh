69730f95e47772f68c1d6475ce0cf30cbd75fa6e
ericgaspar <junk.eg@free.fr>
2021-11-16 20:59:52 +0100

    4.3

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,7 @@
 
 YNH_PHP_VERSION="7.3"
 
-extra_php_dependencies="php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-xml"
+pkg_dependencies="php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-xml"
 
 #=================================================
 # PERSONAL HELPERS
